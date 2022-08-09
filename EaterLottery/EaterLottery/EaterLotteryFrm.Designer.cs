cd43c47846059dcd13434d104026dfb1fb8e93ab
2207004 <chenghao.lin@iisigroup.com>
2022-08-09 09:20:47 +0800

    新增專案

---
+++ EaterLottery/EaterLottery/EaterLotteryFrm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace EaterLottery
+{
+    partial class EaterLotteryFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn.Location = new System.Drawing.Point(62, 53);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(349, 185);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "開抽囉";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // EaterLotteryFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 309);
+            this.Controls.Add(this.btn);
+            this.Name = "EaterLotteryFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "中午吃啥??";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn;
+    }
+}
---
+++ EaterLottery/EaterLottery/EaterLotteryFrm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EaterLottery
+{
+    public partial class EaterLotteryFrm : Form
+    {
+        public EaterLotteryFrm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 取得AppSettings
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns></returns>
+        private static string GetAppSetting(string key)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            return appSettings.AllKeys.Contains(key) ? appSettings[key].ToString() : "";
+        }
+
+        /// <summary>
+        /// 設定AppSettings
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="value">value</param>
+        private static void SetAppSetting(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+            config.AppSettings.Settings[key].Value = value;
+            config.Save();
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        /// <summary>
+        /// 開抽囉
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            string[] stores = GetAppSetting("store").Split(',').OrderBy(x => r.Next()).ToArray();//.OrderBy();            
+            string store; //今天抽的
+            List<string> oldStores = GetAppSetting("oldStore").Split(',', StringSplitOptions.RemoveEmptyEntries).ToList(); //昨天抽的
+            do
+            {
+                //至少抽一次....
+                store = stores[r.Next(0, stores.Length)];
+            }
+            while (oldStores.Contains(store));
+
+            //5家就重置....
+            if (oldStores.Count >= 5)
+                oldStores.Clear();
+
+            //加入設定檔
+            oldStores.Add(store);
+
+            //存檔
+            SetAppSetting("oldStore", string.Join(",", oldStores));
+
+            MessageBox.Show(store, "恭喜抽中");
+        }
+    }
+}
